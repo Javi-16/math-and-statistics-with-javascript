345fef8c3b10de9d31ecaa82fb08cbd0966ea046
Javier Eduardo Quiroga Amaya <jeqa2004.7916@gmail.com>
2023-05-04 15:02:57 -0400

    Agrego las proyecciones de salario. Arreglo el 'return' de la mediana de salarios

--- analisis.js
+++ analisis.js
@@ -17,4 +17,28 @@ function medianaPorPersona(nombrePersona) {
     });
 
     const medianaSalarios = PlatziMath.calcularMediana(salarios);
+
+    return medianaSalarios;
+}
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i < trabajos.length; i++) {
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos[i - 1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = crecimiento / salarioPasado;
+        porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+    
+    return nuevoSalario;
 }
